b480cbdabcbd656fd9c78a51fe91ed2f2cf904f7
Norbert L. Ruehl <norbekaiser@users.noreply.github.com>
2016-03-20 10:47:18 +0100

    Update disp.sh

--- disp.sh
+++ disp.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-SCRIPTNAME=disp.sh
 case $1 in
-        rotate)
-                xrandr --output DVI-I-2 --rotate normal
-                xrandr --output DVI-I-3 --rotate left
-                xrandr --output DVI-I-2 --pos 0x0
-                xrandr --output DVI-I-3 --pos 1920x-250
-        ;;
-        normal)
-                xrandr --output DVI-I-2 --rotate normal
-                xrandr --output DVI-I-3 --rotate normal
-                xrandr --output DVI-I-2 --pos 0x0
-                xrandr --output DVI-I-3 --pos 1920x0
+	rotate)
+		xrandr --output DVI-I-2 --rotate normal
+		xrandr --output DVI-I-3 --rotate left
+		xrandr --output DVI-I-2 --pos 0x0
+		xrandr --output DVI-I-3 --pos 1920x-250
+	;;
+	normal)
+		xrandr --output DVI-I-2 --rotate normal
+		xrandr --output DVI-I-3 --rotate normal
+		xrandr --output DVI-I-2 --pos 0x0
+		xrandr --output DVI-I-3 --pos 1920x0
 
-        ;;
-        *)
-                echo "Usage: $SCRIPTNAME {rotate|normal}" >&2
-                exit 3
-        ;;
+	;;
+	*)
+		echo "Usage: $0 {rotate|normal}" >&2
+		exit 3
+	;;
 esac
